25c4831282ac836f4c7db806a0d7633c689d1a23
Sivakaki <sivakaki033@gmail.com>
2026-08-29 22:09:25 +0530

    add Movie Details page

--- client/src/components/DateSelect.jsx
+++ client/src/components/DateSelect.jsx
@@ -1,7 +1,21 @@
-import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import { ChevronLeftIcon, ChevronRightIcon, NavigationOff } from "lucide-react";
 import { BlurCircle } from "./BlurCircle";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export const DateSelect = ({ dateTime, id }) => {
+  const navigate = useNavigate();
+  const [selcted, setSelcted] = useState(null);
+  const onBookHandler = () => {
+    if (!selcted) {
+      return toast("Please select a date");
+    }
+
+    navigate(`/movies/${id}/${selcted}`);
+    scrollTo(0, 0);
+  };
+
   return (
     <div id="dateSelect" className="pt-30">
       <div className="flex flex-col md:flex-row items-center justify-between gap-10 relative p-8 bg-primary/10 border border-primary/20 rounded-lg">
@@ -13,23 +27,36 @@ export const DateSelect = ({ dateTime, id }) => {
           <div className="flex items-center gap-6 text-sm mt-5">
             <ChevronLeftIcon width={28} />
             <span className="grid grid-cols-3 md:flex-wrap md:max-w-lg gap-4">
-              {Object.keys(dateTime).map((date) => {
-                <button className="flex fleco' items-center justify-items-center h-14 w-14 aspect-square rounded cursor-pointer">
+              {Object.keys(dateTime).map((date) => (
+                <button
+                  key={date}
+                  className={`flex flex-col items-center justify-center h-14 w-14 aspect-square rounded cursor-pointer ${
+                    selcted === date
+                      ? "bg-primary text-white"
+                      : "border border-primary/70"
+                  }`}
+                  onClick={() => setSelcted(date)}
+                >
                   <span>{new Date(date).getDate()}</span>
                   <span>
                     {new Date(date).toLocaleDateString("en-US", {
                       month: "short",
                     })}
                   </span>
-                </button>;
-              })}
+                </button>
+              ))}
             </span>
 
             <ChevronRightIcon width={28} />
           </div>
         </div>
 
-        <button className="bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90 transition-all cursor-pointer">Book Now</button>
+        <button
+          className="bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90 transition-all cursor-pointer"
+          onClick={onBookHandler}
+        >
+          Book Now
+        </button>
       </div>
     </div>
   );
---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,7 @@
+export const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-[80vh]">
+      <div className="animate-spin rounded-full h-1/4 w-1/4 border-2 border-t-primary"></div>
+    </div>
+  );
+};
